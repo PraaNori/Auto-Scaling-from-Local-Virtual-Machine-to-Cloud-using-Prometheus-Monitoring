af1cbdbd3d148d25a16b7690684572ebf3bfca7f
Pradeepika Nori <147644761+PraaNori@users.noreply.github.com>
2026-03-29 01:59:39 +0530

    Add Flask app with scaling trigger endpoint

--- cloud_vm/cloud.py
+++ cloud_vm/cloud.py
@@ -1,1 +1,15 @@
+from flask import Flask
+import os
 
+app = Flask(__name__)
+
+@app.route('/trigger', methods=['POST'])
+def trigger():
+    print("Scaling Trigger Received!")
+
+    # Start app in background
+    os.system("nohup python3 app.py &")
+
+    return "Scaling Done", 200
+
+app.run(host='0.0.0.0', port=80)
